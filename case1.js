5178941b1607fe079434b32bda45918d10637ed5
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-10-07 09:44:52 +0200

    created case 1

---
+++ case1.js
@@ -0,0 +1,24 @@
+/* 
+    Write a function that takes an object with two 
+    properties and a string as arguments
+    It should return the value of the property with 
+    key equal to the value of the string
+*/
+
+function checkKeyContent(obj, key){
+
+    if(obj[key]){
+        return obj[key];
+    }
+
+    return false
+}
+
+console.log(checkKeyContent({  continent: 'Asia',  country: 'Japan'}, 'continent'));
+
+/*
+    The first step in function checkKeyContent is check if there is the key in obj;
+    If obj has this key the function return key's content;
+    If obj does not have this key the funtion return false. So if the function's return is used to initiate a process and 
+    the return is false the process does not happens
+*/
